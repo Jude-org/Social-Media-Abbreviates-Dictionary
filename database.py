e8ad95466fe3ba997b3773ad850e1c3f1bbc64ca
Orangutann <tshimollo9909@gmail.com>
2023-08-20 01:44:00 +0200

    initial commit

---
+++ database.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+def create_database_table(connection):
+    connection = sqlite3.connect('definitions.db')
+    cursor = connection.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS definitions (
+        word TEXT PRIMARY KEY,
+        definition TEXT
+        )
+    ''')
+    
+    connection.commit()
+    connection.close()
+
+def add_definition(word, definition):
+    connection = sqlite3.connect('definitions.db')
+    cursor = connection.cursor()
+    
+    cursor.execute('INSERT INTO definitions (word, definition) VALUES (?, ?)', (word, definition))
+    
+    connection.commit()
+    connection.close()
+
+def get_definition(connection, word):
+    cursor = connection.cursor()
+    cursor.execute(''' SELECT definition FROM definitions WHERE word = ?''', (word,))
+    results = cursor.fetchone()
+    return results[0] if results else None
+
---
+++ main.py
@@ -0,0 +1,38 @@
+import sqlite3
+from database import create_database_table, add_definition, get_definition
+
+def deabbreviate(user_input, connection):
+    uppercase_input = user_input.upper()
+    word = get_definition(connection, uppercase_input)
+    
+    if word:
+        return word
+    else:
+        return "Sorry, that abbreviation is not in our database."
+
+
+def display_welcome_message():
+    motto = "Don't always be caught out of the loop."
+    dashes = len(motto) * '*'
+    print("Welcome to Social Media Abbreviate Deabbreviator!")
+    print(f"{motto}\n{dashes}\n")
+
+
+def main():
+    connection = sqlite3.connect('definitions.db')
+    create_database_table(connection)
+    display_welcome_message()
+
+    while True:
+        user_input = input("Enter your abbreviation (or 'EXIT' to quit): ").upper()
+        
+        if user_input == "EXIT":
+            print("Thank you for using the Abbreviate Deabbreviator. Goodbye!")
+            break
+        
+        result = deabbreviate(user_input, connection)
+        print(result)
+    connection.close()
+    
+if __name__ == "__main__":
+    main()
