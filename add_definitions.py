f5643d9efc55c12648235dbf1c376243b1df7087
Orangutann <tshimollo9909@gmail.com>
2023-08-21 01:30:44 +0200

    Refactor : file manager checks for anomalities in entries before returning the definition and if triggered return none, add to db checks for the return type of the file manager output then executes accordingly, add to db checks if entry exsit in database before executing the main function

--- add_definitions.py
+++ add_definitions.py
@@ -7,12 +7,22 @@ def file_manager():
         for line in file:
             entries += 1
             separated = line.strip().split("-")
-            definitions[f"entry{entries}"] = separated
-    return definitions
+            if len(separated) >= 2:
+                definitions[f"entry{entries}"] = separated
+            else:
+                print("Only one part of the entry is read. Check definitions.txt for anomalities")
+                return None
+        return definitions
 
 def add_to_database(definitions):
-    for entry, (word, definition) in definitions.items():
-        db.add_definition(word, definition)
+    if definitions != None:
+        connection = db.connect_database('definitions.db')
+        for entry, (word, definition) in definitions.items():
+            is_entry_exist = db.get_definition(connection, word)
+            if is_entry_exist:
+                print(f"Skipped {word} because it already exists.")
+            else:
+                db.add_definition(word, definition)
+    connection.close()
     
-        
 add_to_database(file_manager())
